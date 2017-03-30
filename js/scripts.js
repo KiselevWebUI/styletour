54c5659228df9c310c135a3a0d22b1383f75fdba
Andrey Kiselev <web-ui@mail.ru>
2017-03-30 14:20:08 +0300

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+/**
+ * Created by avk on 13.09.2016.
+ */
+$(function(){
+
+  $('.carousel').carousel({
+    interval: false
+  });
+
+  $('.search').on('click', function(){
+    if($('.main-menu .navbar-form').is(':hidden'))$('.main-menu .navbar-form').slideDown();
+    else $('.main-menu .navbar-form').slideUp('normal', function(){
+      $('.main-menu .navbar-form').css('cssText', '');
+    });
+  });
+
+  $('#elastislide').elastislide();
+
+});
